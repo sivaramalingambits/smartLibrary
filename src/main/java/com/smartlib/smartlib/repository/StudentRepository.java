6ab06a3801e57beedf96b81b68fffca0a0e092d0
unknown <rajtony007k@gmail.com>
2026-02-08 18:09:00 +0530

    Revert "slib-12 src implementation"
    This reverts commit cf3d941b07e6c03071350e41a1328e9bcd1e0af7.

--- src/main/java/com/smartlib/smartlib/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartlib.smartlib.repository;
-
-import com.smartlib.smartlib.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-    Student findByEmail(String email);
-}
